95913d8ba9f8afad8bb663cd9ef26b2d43b16bd5
Soujatya Das <soujatya548@gmail.com>
2025-06-04 15:20:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,8 +59,8 @@ if summarize_button and uploaded_file is not None:
             
             1. Overview (brief summary of the document in 6-7 lines - single paragraph)
             2. Involved Parties (key individuals or organizations mentioned divided under Petitioner and Respondents)
-            3. Issues before the Court (show the pointers)
-            4. Observation/Decision of the Court (display the pointers for important rulings or conclusions)
+            3. Issues before the Court (show the pointers, 4-5)
+            4. Observation/Decision of the Court (display the pointers for important rulings or conclusions, 4-5)
             
             {pii_instructions}
             
